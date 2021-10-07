18ce551f66dfd8dc7797fb3dd4b51b297a6f3645
Ewerton  Guimarães <ewertonpg@hotmail.com>
2021-10-06 21:10:03 -0300

    ajustes de projeto

--- Models/Idados.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Models; // agrupamento de classes/interfaces (caminho)
-
-// Interfaces estabelecem os métodos públicos que devem existir nas classes que as implementam, ou seja, métodos que interfaceiam com outras partes de código.
-interface Idados{
-	public function toJson();
-	public function toString();
-}
-?>
