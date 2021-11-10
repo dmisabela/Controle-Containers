80ebb8943247c011555f93eb805500498032f5a4
Isabela <duartemorenoisabela@gmail.com>
2021-11-09 23:07:04 -0300

    Fix header names

--- Views/footer.php
+++ Views/footer.php
@@ -1,7 +1,9 @@
 <div class="jumbotron text-center" style="margin-bottom:0">
     <p>Autores: </p>
     <p>Ewerton Guimarães </p>
-    <p>Isabela de Oliveira </p>
+    <p>Isabela Duarte </p>
+    <p>Arthur Andrade </p>
+    <p>João Guilherme </p>
 </div>
 </body>
 
