5a17d23d848059b4d27c7613da31e7d02408e3ce
Isabela <duartemorenoisabela@gmail.com>
2021-11-09 23:05:55 -0300

    Feita paginação da lista de navio

--- Views/Navio/navio-index.php
+++ Views/Navio/navio-index.php
@@ -8,21 +8,100 @@ spl_autoload_register(function ($class_name) {
 
 <h4>Navios</h4>
 <a href="navio-create.php" class="btn btn-primary btn-small">Novo Navio</a>
+
+<?php
+use Db\Persiste;
+use Models\Navio;
+$navios = Persiste::GetAll('Models\Navio');
+
+try {
+
+    $pdo = new PDO(hostDb,usuario,senha);
+
+    // Configura o comportamento no caso de erros: levanta exceção.
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Não emula comandos preparados, usa nativo do driver do banco
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+
+    $total = $pdo->query('
+        SELECT
+            COUNT(*)
+        FROM
+            navios
+    ')->fetchColumn();
+
+    $limit = 3;
+    $pages = ceil($total / $limit);
+
+    $page = min($pages, filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT, array(
+        'options' => array(
+            'default'   => 1,
+            'min_range' => 1,
+        ),
+    )));
+
+    $offset = ($page - 1)  * $limit;
+
+    $start = $offset + 1;
+    $end = min(($offset + $limit), $total);
+
+    $prevlink = ($page > 1) ? '<a href="?page=1" title="First page">&laquo;</a> <a href="?page=' . ($page - 1) . '" title="Previous page">&lsaquo;</a>' : '<span class="disabled">&laquo;</span> <span class="disabled">&lsaquo;</span>';
+
+    $nextlink = ($page < $pages) ? '<a href="?page=' . ($page + 1) . '" title="Next page">&rsaquo;</a> <a href="?page=' . $pages . '" title="Last page">&raquo;</a>' : '<span class="disabled">&rsaquo;</span> <span class="disabled">&raquo;</span>';
+
+
+    $stmt = $pdo->prepare('
+        SELECT
+            *
+        FROM
+            navios
+        ORDER BY
+            id
+        LIMIT
+            :limit
+        OFFSET
+            :offset
+    ');
+
+    $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+    $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0) {
+
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $iterator = new IteratorIterator($stmt);
+
+    } else {
+        echo '<p>No results could be displayed.</p>';
+    }
+
+} catch (Exception $e) {
+    echo '<p>', $e->getMessage(), '</p>';
+}
+
+?>
+
 <table class="table table-striped" style="margin-top: 5px">
     <tr><th>ID</th><th>Nome do Navio</th><th>Viagem do Navio</th><th></th><th></th></tr>
     <?php
-    use Db\Persiste;
-    use Models\Navio;
-    $navios = Persiste::GetAll('Models\Navio');
 
+    foreach($iterator as $table){
 
-    foreach($navios as $p){
-        echo "<tr><td>$p->getID</td><td>$p->getNOME</td><td>$p->getNUM_VIAGEM</td>"
-            ."<td><a href='navio-edit.php?id=$p->getID' class='btn btn-primary btn-small'>Editar</a></td>"
-            ."<td><a href='navio-delete.php?id=$p->getID' class='btn btn-primary btn-small'>Excluir</a></td></tr>";
+        $id = $table['ID'];
+        $nome = $table['NOME'];
+        $nv = $table['NUM_VIAGEM'];
+
+        echo "<tr><td>$id</td><td>$nome</td><td>$nv</td>"
+            ."<td><a href='navio-edit.php?id=$id' class='btn btn-primary btn-small'>Editar</a></td>"
+            ."<td><a href='navio-delete.php?id=$id' class='btn btn-primary btn-small'>Excluir</a></td></tr>";
     }
-
     ?>
 </table>
+
+<?php
+echo '<div style="text-align:center"><p>', $prevlink, ' Page ', $page, ' of ', $pages, ' pages, displaying ', $start, '-', $end, ' of ', $total, ' results ', $nextlink, ' </p></div>';
+?>
 
 <?php include '..\footer.php'; ?>
